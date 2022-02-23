66d49a2de8e73998186a01c933836c653cac9c56
ScubaCreator <65056761+ScubaCreator@users.noreply.github.com>
2022-02-22 21:41:23 -0800

    upload cheesus to the masses
    cheese

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome To Cheesus")
+print("enter any bible verse containing God or Jesus and watch it become cheese")
+print("Enter your bible verse")
+bible = input("")
+
+bible = (bible.replace("Jesus","Cheesus"))
+bible = (bible.replace("God", "Gouda"))
+
+print(bible)
